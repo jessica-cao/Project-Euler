1dc81d8131448556b1d7b67b3bdf7ec20d377100
jessica-cao <jessicacao1756@gmail.com>
2019-06-20 23:55:19 -0400

    up to p02

---
+++ p01-p10/p01.java
@@ -0,0 +1,22 @@
+import java.math.*;
+
+public class p01 {
+
+    public static int sumOfMultiplesOf3And5Under1000() {
+        int sum = 0;
+
+        for(int i = 1; i < 1000; ++i) {
+            if (i % 3 == 0 | i % 5 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println(sum);
+        return sum;
+
+    }
+
+    public static void main(String[] args) {
+        sumOfMultiplesOf3And5Under1000();
+    }
+
+}
